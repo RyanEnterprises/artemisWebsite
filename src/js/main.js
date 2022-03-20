750a496f60be9703d869810c9ebecd8bc6faaa07
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-03-20 19:45:13 -0400

    made first module!

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,4 @@
-
+import { DEVICE_type } from './UAdetect';
+if (DEVICE_type == "mobile") {
+    document.getElementById("nav").style.display = "none";
+}
---
+++ src/ts/main.ts
@@ -0,0 +1,5 @@
+import { ORIENTATION_isLandscape, DEVICE_type } from './UAdetect';
+
+if (DEVICE_type == "mobile") {
+    document.getElementById("nav").style.display = "none";
+}
