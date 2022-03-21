f14813b8862ac3ae63c3096e313e3a10d013c728
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-03-20 20:28:44 -0400

    made first side prototype

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
 import { DEVICE_type } from './UAdetect';
+import 'C:/Users/rpmul/Documents/artemisWebsite/src/css/style.css';
+import 'C:/Users/rpmul/Documents/artemisWebsite/src/css/fonts.css';
 if (DEVICE_type == "mobile") {
     document.getElementById("nav").style.display = "none";
 }
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,8 @@
 import { ORIENTATION_isLandscape, DEVICE_type } from './UAdetect';
 
+import 'C:/Users/rpmul/Documents/artemisWebsite/src/css/style.css'
+import 'C:/Users/rpmul/Documents/artemisWebsite/src/css/fonts.css'
+
 if (DEVICE_type == "mobile") {
     document.getElementById("nav").style.display = "none";
 }
