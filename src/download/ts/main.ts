5cf05ea0c0a2f6373efe8fb9845766a53c58c38f
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-03-22 15:42:49 -0400

    feat: :sparkles: took out the navbar for mobile devices

--- src/download/ts/main.ts
+++ src/download/ts/main.ts
@@ -1,2 +1,17 @@
+// Imports
+// Stylesheets
 import 'C:/Users/rpmul/Documents/artemisWebsite/src/css/style.css';
 import 'C:/Users/rpmul/Documents/artemisWebsite/src/css/fonts.css';
+
+// From the user agent
+import { DEVICE_type } from '../../ts/UAdetect';
+
+// From the declaration file
+
+import { nav, header, headerLogo } from '../../ts/indexDeclarations';
+
+// Function to hide nav bar on mobile
+
+if (DEVICE_type === 'mobile') {
+    nav.style.display = 'none';   
+}
